edc5b0c1403d65a3b923c104631dff7b154a7564
Leonardo <leosr17@gmail.com>
2024-10-19 18:19:19 -0500

    Cambio 2
    Signed-off-by: Leonardo <leosr17@gmail.com>

--- while.py
+++ while.py
@@ -6,7 +6,7 @@ while nro != 0:
     total+=nro
     nro=int(input("Número: "))"""
 
-# Leer números enteros de teclado, hasta que el usuario ingrese el 0. Finalmente, mostrar la sumatoria de todos los números positivos ingresados.
+# 2 Leer números enteros de teclado, hasta que el usuario ingrese el 0. Finalmente, mostrar la sumatoria de todos los números positivos ingresados.
 
 """positivos=0
 n=int(input("Número (0 para terminar): "))
