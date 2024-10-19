b8d0ec88c5ca67ee147afec211a9a2ecafdef662
7eonard <148588758+7eonard@users.noreply.github.com>
2024-10-19 16:14:33 -0500

    Update while.py

--- while.py
+++ while.py
@@ -1,5 +1,5 @@
 
-# Leer números enteros de teclado, hasta que el usuario ingrese el 0. Finalmente, mostrar la sumatoria de todos los números ingresados.
+# 1 Leer números enteros de teclado, hasta que el usuario ingrese el 0. Finalmente, mostrar la sumatoria de todos los números ingresados.
 total=0
 nro=int(input("Número: "))
 while nro != 0:
