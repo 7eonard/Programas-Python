ff969870a956d2edd9e1b9b6f2bbd60cafbb4abe
Leonardo <leosr17@gmail.com>
2024-10-19 18:16:55 -0500

    cambio 1

--- while.py
+++ while.py
@@ -6,6 +6,16 @@ while nro != 0:
     total+=nro
     nro=int(input("Número: "))"""
 
+# Leer números enteros de teclado, hasta que el usuario ingrese el 0. Finalmente, mostrar la sumatoria de todos los números positivos ingresados.
+
+"""positivos=0
+n=int(input("Número (0 para terminar): "))
+while n!=0:
+    if n>0:
+        positivos+=1
+    n=int(input("Número (0 para terminar): "))
+print("Cantidad de positivos:", positivos)"""
+
 
 
 pares=0
