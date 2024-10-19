900411ee96b7fcf61c0db57c2de02b8916bcbbba
Leonardo <leosr17@gmail.com>
2024-10-19 15:58:50 -0500

    Feat: While #1

--- while.py
+++ while.py
@@ -1,3 +1,13 @@
+
+# Leer números enteros de teclado, hasta que el usuario ingrese el 0. Finalmente, mostrar la sumatoria de todos los números ingresados.
+total=0
+nro=int(input("Número: "))
+while nro != 0:
+    total+=nro
+    nro=int(input("Número: "))
+
+
+
 pares=0
 n=int(input("Número (-1 para terminar el programa): "))
 while n!=-1:
